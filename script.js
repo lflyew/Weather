a6ed8fc8b6a469e40cafd0a91cab950fdc7783bb
Latoya Lyew <nursebae@Latoyas-MacBook-Pro.local>
2022-08-16 16:58:36 -0400

    search city function

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 localStorage.clear;
-
+// Variables created
 var API key = '558872b4719f78b0116470aa2fdbdd69'
 var searchboxEl = document.querySelector("#search-box");
 var citysearchEl = document.querySelector("#city");
@@ -15,8 +15,20 @@ var weathericon = document.querySelector("#weather-icon");
 var currenticon = document.querySelector("#current-icon");
 var wind = document.querySelector("#wind");
 
-function find(){
+var search = JSON.parse(localStorage.getItem("search") || "[]");
+//Search city
+var formSumbit = function (event){
+    event.preventDefault();
     var city = titleCase ($("#city")[0].value.trim());
-
-    var API_Url = "https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}"
-}
+    citysearchEl.value = ""
+//Error message if city name not entered
+        if (city) {
+            getCoordinates(city);
+            error.innerHTML = ""
+        } else{
+            error.innerHTML = "Please enter City"
+        
+            return;
+        }
+        }
+    
